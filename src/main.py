34b0df9f828cd6e915c261314836177ba73a70ef
Matej Kubinec <matej.kubinec@outlook.com>
2020-05-25 20:52:17 +0200

    Added distributions

--- src/main.py
+++ src/main.py
@@ -48,36 +48,34 @@ cols = {
 }
 
 # TODO: FIX WARNING: sys:1: DtypeWarning: Columns (28) have mixed types.Specify dtype option on import or set low_memory=False.
-train_data = pd.read_csv('data/train.csv', index_col=0, header=0)
-train_data.describe().to_csv('results/non_categorical_description.csv')
-train_data.head().to_csv('results/head.csv')
-
-len(train_data)
+train_df = pd.read_csv('data/train.csv', index_col=0, header=0)
+train_df.describe().to_csv('results/non_categorical_description.csv')
+train_df.head().to_csv('results/head.csv')
 
 # Null values
-null_df = pd.DataFrame(data=train_data.isnull().sum(axis=0)).transpose()
+null_df = pd.DataFrame(data=train_df.isnull().sum(axis=0)).transpose()
 null_df.to_csv('results/null_values.csv')
 
 null_non_zero_df = null_df.T[null_df.T > 0].dropna().transpose()
 null_non_zero_df.to_csv('results/null_values_non_zero.csv')
 
-# Remove dots in version columns
-for col in cols['version']:
-    train_data[col] = train_data[col].str.replace('.', '')
-
 # Factorize categorical columns
-for col in cols['categorical']:
-    codes, uniques = pd.factorize(train_data[col])
-    train_data[col] = codes
+for col in cols['version']:
+    codes, uniques = pd.factorize(train_df[col])
+    train_df[col] = codes
 
-train_data.head().to_csv('results/head_cleaned.csv')
+for col in cols['categorical']:
+    codes, uniques = pd.factorize(train_df[col])
+    train_df[col] = codes
+
+train_df.head().to_csv('results/head_cleaned.csv')
 
 non_categorical_cols_to_drop = [cols['result']] + cols['categorical']
-non_categorical = train_data.drop(columns=non_categorical_cols_to_drop)
+non_categorical = train_df.drop(columns=non_categorical_cols_to_drop)
 
 non_categorical.describe().to_csv('results/non_categorical_description.csv')
 
-corr_mat = train_data.corr()
+corr_mat = train_df.corr()
 corr_mat.to_csv('results/corr_mat.csv')
 
 # Plot correlation matrix
@@ -94,7 +92,9 @@ corr_ax.set_yticklabels(corr_ax.get_yticklabels(), rotation=0)
 corr_ax.set_xticklabels(corr_ax.get_xticklabels(), rotation=90)
 corr_fig.savefig('images/corr.png')
 
+#
 # Plot null values
+#
 null_fig = Figure(figsize=(25, 10))
 null_fig.tight_layout()
 null_ax = null_fig.add_subplot(111)
@@ -104,7 +104,9 @@ sns.barplot(ax=null_ax, x=null_x, y=null_y, palette=cmap)
 null_ax.set_xticklabels(null_ax.get_xticklabels(), rotation=90)
 null_fig.savefig('images/null_values.png')
 
+#
 # Plot null values - non-zero
+#
 null_non_zero_fig = Figure(figsize=(25, 10))
 null_non_zero_fig.tight_layout()
 null_non_zero_ax = null_non_zero_fig.add_subplot(111)
@@ -116,7 +118,7 @@ null_non_zero_ax.set_xticklabels(
     null_non_zero_ax.get_xticklabels(), rotation=90)
 null_non_zero_fig.savefig('images/null_values_non_zero.png')
 
-corr_sub_df = train_data[[
+corr_sub_df = train_df[[
     'OsVer',
     'OsBuild',
     'IeVerIdentifier',
@@ -158,22 +160,24 @@ cols_to_drop = [
     'DefaultBrowsersIdentifier',
     'OrganizationIdentifier',
     'PuaMode',
-    'ProcessorClass',
+    'Census_ProcessorClass',
     'Census_PowerPlatformRoleName',
     'Census_InternalBatteryType',
-    'Census_TresholdOptIn',
-    'Census_WIMBootEnabled',
+    'Census_ThresholdOptIn',
+    'Census_IsWIMBootEnabled',
 ]
 
-corr_mat = None
-train_df = train_data.drop(columns=cols_to_drop)
-train_data = None
+#
+# Correlation Matrix - Cleaned Up
+#
+train_df = train_df.drop(columns=cols_to_drop)
+train_corr = train_df.corr()
 
 corr_fig = Figure(figsize=(75, 50))
 corr_fig.tight_layout()
 corr_ax = corr_fig.add_subplot(111)
 sns.heatmap(
-    train_df,
+    train_corr,
     ax=corr_ax,
     annot=True,
     cmap=cmap,
@@ -181,3 +185,24 @@ sns.heatmap(
 corr_ax.set_yticklabels(corr_ax.get_yticklabels(), rotation=0)
 corr_ax.set_xticklabels(corr_ax.get_xticklabels(), rotation=90)
 corr_fig.savefig('images/cleaned_corr.png')
+
+#
+# Distributions
+#
+column_names = train_df.columns.tolist()
+
+dist_fig = Figure(figsize=(50, 50))
+dist_fig.tight_layout()
+dist_axs = dist_fig.subplots(9, 8)
+
+for i, col_name in enumerate(column_names):
+    r = int(i / 8)
+    c = int(i % 8)
+
+    ax = dist_axs[r, c]
+
+    hist_kws = {'color': '#a4d4bc'}
+    kde_kws = {'shade': True, 'color': '#0b1e56'}
+    sns.distplot(train_df[col_name], ax=ax, kde_kws=kde_kws, hist_kws=hist_kws)
+
+dist_fig.savefig('images/distributions.png')
